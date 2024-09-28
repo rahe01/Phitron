69e2c5210ba38c1e0914695ed6f6d05c64aabc5e
Rahe <130391604+rahe01@users.noreply.github.com>
2024-09-28 22:30:08 +0600

    first code in c

---
+++ Introduction to Programming Language/Week-1/First.c
@@ -0,0 +1,15 @@
+// This is my first code
+
+/*
+This is my first code in phitron
+
+*/
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello, Phitron!\n");
+    printf("This is my first code");
+
+    return 0;
+}
---
+++ Introduction to Programming Language/Week-1/variable.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main (){
+
+    // int a , b , c;
+    // scanf ("%d %d %d", &a , &b , &c);
+    // char ch;
+    // // getchar ();
+    // scanf ("\n %c" , &ch);
+
+
+ 
+
+    // printf("Inter a number a = %d b = %d c = %d ch = %c" , a, b, c , ch);
+
+    float f;
+
+    scanf("%f" , &f);
+    printf("Enter a number %.3f" , f);
+
+
+
+
+
+
+    return 0;
+}
+
+/*
+Formate specifier
+
+
+short int %hd
+long int %ld
+long long %lld
+int %d 
+char %c
+float %f
+double %lf
+string %s
+longdouble %Lf
+unsigned int %u
+unsigned short int %hu
+
+
+*/
