d57d7ef57b08b9a0eb5443f03898998eed4c5cf7
Rahe <130391604+rahe01@users.noreply.github.com>
2024-10-01 14:00:41 +0600

    d

---
+++ Introduction to Programming Language/Week-1/Operators, Conditional Statements/practice.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+int main () {
+
+    int a;
+    scanf("%d" , &a);
+
+    if (a < 5) {
+        printf("a is less than 5");
+    }
+    
+
+
+
+
+
+
+    return 0;
+}
