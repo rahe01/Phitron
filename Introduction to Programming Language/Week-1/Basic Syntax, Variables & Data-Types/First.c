8e62c4e37b0cedbe9ea25f6cc7bb2a9a2ce1ec3b
Rahe <130391604+rahe01@users.noreply.github.com>
2024-10-04 00:30:33 +0600

    .

--- Introduction to Programming Language/Week-1/Basic Syntax, Variables & Data-Types/First.c
+++ Introduction to Programming Language/Week-1/Basic Syntax, Variables & Data-Types/First.c
@@ -1,15 +1,15 @@
-// This is my first code
-
-/*
-This is my first code in phitron
-
-*/
-
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
-int main(){
-    printf("Hello, Phitron!\n");
-    printf("This is my first code");
+int main() {
+    
+    int A , B     ;
+    scanf("%d %d" , &A , &B);
+    
+    printf("%d " , A*B);
 
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     return 0;
 }
