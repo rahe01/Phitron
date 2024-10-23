9c7daa0be8439569b0505766ef55aa9b3f260843
Rahe <130391604+rahe01@users.noreply.github.com>
2024-10-23 13:58:21 +0600

    h

---
+++ Introduction to Programming Language/Week-3/String_opretaion/copy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char a[10];
+    char b[10] = "abcdef";
+
+    int length = strlen();
+
+    for (int i = 0; b[i] != '\0'; i++)
+    {
+        a[i] = b[i];
+        /* code */
+    }
+    
+
+    printf("%s %s\n", a, b);
+
+
+
+    return 0;
+}
---
+++ Introduction to Programming Language/Week-3/String_opretaion/length.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char str[10] = "abcdyy yy";
+
+    int length = strlen(str);
+
+
+    // for (int i = 0; str[i] != '\0'; i++)
+    // {
+    //     length++;
+    //     /* code */
+    // }
+
+    printf("%d" , length);
+    
+
+
+
+
+    return 0;
+}
