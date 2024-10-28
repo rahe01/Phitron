dd8a46a424fb6b93106b03215f486cce9fe47f2c
Rahe <130391604+rahe01@users.noreply.github.com>
2024-10-29 01:01:07 +0600

    c

--- Introduction to Programming Language/Week-4/Practise1/MatrixSum.c
+++ Introduction to Programming Language/Week-4/Practise1/MatrixSum.c
@@ -1,24 +1,46 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+int main()
+{
 
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
 
     int a[n][n];
 
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            scanf("%d ",&a[i][j]);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
         }
     }
 
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            scanf("%d ",&a[i][j]);
+    int main = 0;
+    int secondary = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+
+                main += a[i][j];
+            }
+
+            if (i + j == n - 1)
+            {
+                secondary += a[i][j];
+                /* code */
+            }
         }
     }
+
+   
+
+    printf("%d" , abs(secondary -main));
 
 
 
