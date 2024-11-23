1a13d05707b242dfc4c4f51a00646829ca9e4a89
Rahe <130391604+rahe01@users.noreply.github.com>
2024-11-23 21:58:56 +0600

    d

---
+++ C++ For DSA/Week-2/StorageClass.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void fun(){
+
+    auto a = 'R';//+
+    auto b = 'A';
+    
+    auto sum = a + b;
+    
+    cout << "Sum is: " << sum << endl;
+
+
+}
+
+
+
+int main(){
+    fun();
+    
+    return 0;
+}
---
+++ C++ For DSA/Week-2/String/String.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  
+    string s = "Hello world ";
+    s.clear();
+   
+
+
+
+
+    cout << s << endl;
+    // cout << s.max_size() << endl;
+    // cout << s.capacity() << endl;
+
+
+
+
+
+
+    return 0;
+}
