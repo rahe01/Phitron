b5d8db7c20f99024012132c7a54294ca02875a6a
Rahe <130391604+rahe01@users.noreply.github.com>
2025-02-22 22:01:12 +0600

    bell

--- Algoritm/Week-1/Breadth First Search (BFS)/a.cpp
+++ Algoritm/Week-1/Breadth First Search (BFS)/a.cpp
@@ -2,52 +2,54 @@
 
 using namespace std;
 
-vector<int> adj[1005];
-bool visited[1005];
-
+vector<int> adj[1000];
+bool visited[1000];
 
 
 
 void bfs(int src){
     queue<int> q;
+
     q.push(src);
+
     visited[src] = true;
 
     while(!q.empty()){
         int parent = q.front();
         q.pop();
-
         cout << parent << " ";
-        for(auto child: adj[parent]){
 
-            if(visited[child] == false){
+        for(auto child : adj[parent]){
+            if(!visited[child]){
                 q.push(child);
                 visited[child] = true;
             }
         }
     }
+
 }
 
 
 
 
 
-
 int main(){
-    int n, e;
+    int n,e;
+
     cin >> n >> e;
 
     while(e--){
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        int a , b;
+        cin >> a>>b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
     }
 
-    memset(visited , false, sizeof(visited));
-    int src;
-    cin >> src;
-    bfs(src);
+
+    memset(visited , false , sizeof(visited));
+
+    bfs(0);
+
 
 
     return 0;
